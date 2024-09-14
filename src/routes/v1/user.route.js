0e009f86afc52ac02b469360c6a18de8ee573f66
ajuk07 <ajaybhandari0064@gmail.com>
2024-09-14 16:04:56 +0530

    added middle to aggregate params

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -3,19 +3,25 @@ const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const userValidation = require('../../validations/user.validation');
 const userController = require('../../controllers/user.controller');
+const aggregateRequestDataMiddleware = require('../../middlewares/requestParameterHandler');
 
 const router = express.Router();
 
 router
   .route('/')
-  .post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser)
-  .get(auth('getUsers'), validate(userValidation.getUsers), userController.getUsers);
+  .post(auth('manageUsers'), aggregateRequestDataMiddleware, validate(userValidation.createUser), userController.createUser)
+  .get(auth('getUsers'), aggregateRequestDataMiddleware, validate(userValidation.getUsers), userController.getUsers);
 
 router
   .route('/:userId')
-  .get(auth('getUsers'), validate(userValidation.getUser), userController.getUser)
-  .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
-  .delete(auth('manageUsers'), validate(userValidation.deleteUser), userController.deleteUser);
+  .get(auth('getUsers'), aggregateRequestDataMiddleware, validate(userValidation.getUser), userController.getUser)
+  .patch(auth('manageUsers'), aggregateRequestDataMiddleware, validate(userValidation.updateUser), userController.updateUser)
+  .delete(
+    auth('manageUsers'),
+    aggregateRequestDataMiddleware,
+    validate(userValidation.deleteUser),
+    userController.deleteUser
+  );
 
 module.exports = router;
 
